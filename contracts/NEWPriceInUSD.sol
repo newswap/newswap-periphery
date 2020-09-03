a287eb858ee4f8352cfafe5ca00392fb962edfab
liuyong <liuyong5653@163.com>
2020-09-03 21:20:05 +0800

    docs: add WNewABI.json

---
+++ contracts/NEWPriceInUSD.sol
@@ -0,0 +1,26 @@
+pragma solidity =0.6.6;
+
+contract NEWPriceInUSD {
+  // address public owner;
+  // mapping(address => bool)) public sources;
+  // mapping(address => Datum)) private data;
+
+  // struct Datum {
+  //   uint64 timestamp;
+  //   string value;
+  // }
+  // constructor() public {
+  //   owner = msg.sender;
+  // }
+
+  // modifier restricted() {
+  //   if (msg.sender == owner) _;
+  // }
+
+
+
+
+  // function setCompleted(uint completed) public restricted {
+  //   last_completed_migration = completed;
+  // }
+}
